1c3d7619fd404ce75194bf54e7cbdaba7988a376
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2021-03-15 13:26:59 -0700

    create subfolders

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,10 @@
-/// <reference path="drag-drop-interfaces.ts" />
-/// <reference path="project-model.ts" />
-/// <reference path="project-state.ts" />
+/// <reference path="models/drag-drop.ts" />
+/// <reference path="models/project-model.ts" />
+/// <reference path="state/project-state.ts" />
+/// <reference path="util/validation.ts" />
+/// <reference path="decorators/autobind.ts" />
 
 namespace App {
-    // Validation
-    interface Validatable {
-        value: string | number;
-        required?: boolean;
-        minLength?: number;
-        maxLength?: number;
-        min?: number;
-        max?: number;
-    }
-
-    function validate(validatableInput: Validatable) {
-        let isValid = true;
-        if (validatableInput.required) {
-            isValid =
-                isValid &&
-                validatableInput.value.toString().trim().length !== 0;
-        }
-        if (
-            validatableInput.minLength != null &&
-            typeof validatableInput.value === 'string'
-        ) {
-            isValid =
-                isValid &&
-                validatableInput.value.length >= validatableInput.minLength;
-        }
-        if (
-            validatableInput.maxLength != null &&
-            typeof validatableInput.value === 'string'
-        ) {
-            isValid =
-                isValid &&
-                validatableInput.value.length <= validatableInput.maxLength;
-        }
-        if (
-            validatableInput.min != null &&
-            typeof validatableInput.value === 'number'
-        ) {
-            isValid = isValid && validatableInput.value >= validatableInput.min;
-        }
-        if (
-            validatableInput.max != null &&
-            typeof validatableInput.value === 'number'
-        ) {
-            isValid = isValid && validatableInput.value <= validatableInput.max;
-        }
-        return isValid;
-    }
-
-    // autobind decorator
-    function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-        const originalMethod = descriptor.value;
-        const adjDescriptor: PropertyDescriptor = {
-            configurable: true,
-            get() {
-                const boundFn = originalMethod.bind(this);
-                return boundFn;
-            }
-        };
-        return adjDescriptor;
-    }
-
     // Component Base Class
     abstract class Component<T extends HTMLElement, U extends HTMLElement> {
         templateElement: HTMLTemplateElement;
